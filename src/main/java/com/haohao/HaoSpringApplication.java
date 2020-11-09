2fa7cbc4070880d511e4ddbba871332717e166c5
haohao-wh <w1490721983@163.com>
2020-11-09 10:01:53 +0800

    first commit

---
+++ src/main/java/com/haohao/HaoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.haohao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haohao/HaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haohao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
