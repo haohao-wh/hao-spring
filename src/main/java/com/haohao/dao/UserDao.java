50beba4d7b14445a995fc5b6a20e241a034e8b16
haohao-wh <w1490721983@163.com>
2021-01-14 10:46:04 +0800

    update target

---
+++ src/main/java/com/haohao/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.haohao.dao;
+
+/**
+ * @ClassNmae UserDao
+ * @Discription
+ * @Author wh
+ * @Date 2020/11/9  11:16
+ * @Version 1.0
+ */
+public interface UserDao {
+}
--- src/test/java/com/haohao/HaoSpringApplicationTests.java
+++ src/test/java/com/haohao/HaoSpringApplicationTests.java
@@ -3,11 +3,12 @@ package com.haohao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HaoSpringApplication.class)
 class HaoSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(123);
 	}
 
 }
